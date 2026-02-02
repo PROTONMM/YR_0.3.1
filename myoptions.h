3ae377531c70f39247f6f7785894c17a53998fd8
Mariusz Ksiezak <mariusz.ksiezak@gmail.com>
2026-02-02 15:12:01 +0100

    added myoptions.h

---
+++ myoptions.h
@@ -0,0 +1,25 @@
+#ifndef myoptions_h
+#define myoptions_h
+
+#define ARDUINO_ESP32S3_DEV
+
+#define ESP32
+
+#define BITRATE_FULL true
+#define SSD1322_GRAYSCALE         true
+#define L10N_LANGUAGE      RU
+#define DSP_MODEL     DSP_SSD1322
+#define TFT_DC      10
+#define TFT_CS      9
+#define TFT_RST     -1
+#define BRIGHTNESS_PIN      255
+#define I2S_DOUT      48
+#define I2S_BCLK      47
+#define I2S_LRC     21
+#define VS1053_CS     255
+#define ENC_BTNR      6
+#define ENC_BTNL      5
+#define ENC_BTNB      7
+#define ENC_INTERNALPULLUP      false
+#define ENC_HALFQUARD         true 
+#endif
